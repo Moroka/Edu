d4b8043ee9d5fe6255867455cfdcb4688fa1edda
Moroka <supermoroka@gmail.com>
2019-07-21 20:28:32 +0300

    remake algos, add tests

---
+++ src/main/java/Edu/MergeSort.java
@@ -0,0 +1,63 @@
+package Edu;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class MergeSort {
+    public static int[] mergeSort(int[] arr) {
+        System.out.print("MergeSort: " + Arrays.toString(arr) + " -> ");
+
+        if (arr.length < 2) {
+            System.out.println(Arrays.toString(arr));
+            return arr;
+        }
+
+        int middle = arr.length / 2;
+
+        int[] leftPartArray = Arrays.copyOfRange(arr, 0, middle);
+        int[] rightPartArray = Arrays.copyOfRange(arr, middle, arr.length);
+
+        System.out.println(Arrays.toString(rightPartArray) + " + " + Arrays.toString(leftPartArray));
+
+        int[] sortedLeft = mergeSort(leftPartArray);
+        int[] sortedRight = mergeSort(rightPartArray);
+
+        return merge(sortedLeft, sortedRight);
+    }
+
+    private static int[] merge(int[] leftArray, int[] rightArray) {
+        System.out.print("Merge: " + Arrays.toString(leftArray) + " + " + Arrays.toString(rightArray) + " -> ");
+
+        ArrayList<Integer> sortedArray = new ArrayList<>();
+        int leftArrayIndex = 0;
+        int rightArrayIndex = 0;
+
+        while ((leftArrayIndex < leftArray.length) && (rightArrayIndex < rightArray.length)) {
+            if (leftArray[leftArrayIndex] < rightArray[rightArrayIndex]) {
+                sortedArray.add(leftArray[leftArrayIndex]);
+                leftArrayIndex++;
+            } else {
+                sortedArray.add(rightArray[rightArrayIndex]);
+                rightArrayIndex++;
+            }
+        }
+
+        // Ugly hack, no ideas how do it better
+        while (leftArrayIndex < leftArray.length) {
+            sortedArray.add(leftArray[leftArrayIndex]);
+            leftArrayIndex++;
+        }
+        while (rightArrayIndex < rightArray.length) {
+            sortedArray.add(rightArray[rightArrayIndex]);
+            rightArrayIndex++;
+        }
+
+        System.out.println(sortedArray);
+
+        int[] arrayForConversion = new int[sortedArray.size()];
+        for (int i =0; i < sortedArray.size(); i++)
+            arrayForConversion[i] = sortedArray.get(i);
+
+        return arrayForConversion;
+    }
+}
---
+++ src/main/java/Edu/QuickSort.java
@@ -0,0 +1,52 @@
+package Edu;
+
+import java.util.Arrays;
+
+public class QuickSort {
+    public static void quickSort(int[] arr, int low, int high) {
+        if (low >= high) return;
+
+        int p = partition(arr, low, high);
+        System.out.println("Partition is: " + Integer.toString(p));
+        quickSort(arr, low, p);
+        quickSort(arr, p + 1, high);
+    }
+
+    private static int partition(int[] arr, int low, int high) {
+        System.out.println("---------------------------------------------------");
+        System.out.println("New partition. Index range: " + Integer.toString(low) + " - " +  Integer.toString(high));
+        printRangeOfArray(arr, low, high);
+        int pivot = arr[(low + high) / 2];
+        System.out.println("Pivot: " + Integer.toString(pivot));
+        int i = low;
+        int j = high;
+
+
+        while (true) {
+            while (arr[i] < pivot) i++;
+            while (arr[j] > pivot) j--;
+
+            System.out.println("First element which most then pivot(or pivot): " + Integer.toString(arr[i]));
+            System.out.println("First element which less then pivot(or pivot): " + Integer.toString(arr[j]));
+            if (arr[i] >= arr[j]) {
+               int temp = arr[i];
+               arr[i] = arr[j];
+               arr[j] = temp;
+               System.out.println("Swap " + Integer.toString(arr[i]) + " -> " + Integer.toString(arr[j]));
+               System.out.println("New array is: " + Arrays.toString(arr));
+            }
+
+            if (i >= j)
+            return j;
+        }
+
+    }
+
+    private static void printRangeOfArray(int[] arr, int low, int high) {
+        System.out.print("Array range: ");
+        for (int i = low; i <= high; ++i) {
+            System.out.print(Integer.toString(arr[i]) + " ");
+        }
+        System.out.println();
+    }
+}
