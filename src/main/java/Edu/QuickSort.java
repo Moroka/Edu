17629e4423462b05273faa10f78d4647af26d977
Moroka <supermoroka@gmail.com>
2019-07-23 11:13:42 +0300

    remake qSort algo

--- src/main/java/Edu/QuickSort.java
+++ src/main/java/Edu/QuickSort.java
@@ -1,6 +1,5 @@
 package Edu;
 
-import java.util.Arrays;
 
 public class QuickSort {
 
@@ -14,38 +13,46 @@ public class QuickSort {
 
         int p = partition(arr, low, high);
         System.out.println("Partition is: " + Integer.toString(p));
+
         doQuickSort(arr, low, p);
         doQuickSort(arr, p + 1, high);
     }
 
-    private static int partition(int[] arr, int low, int high) {
+    private static int partition(int []arr, int low, int high) {
         System.out.println("---------------------------------------------------");
         System.out.println("New partition. Index range: " + Integer.toString(low) + " - " +  Integer.toString(high));
         printRangeOfArray(arr, low, high);
+
         int pivot = arr[(low + high) / 2];
         System.out.println("Pivot: " + Integer.toString(pivot));
-        int i = low;
-        int j = high;
 
+        int i = low - 1;
+        int j = high + 1;
 
         while (true) {
-            while (arr[i] < pivot) i++;
-            while (arr[j] > pivot) j--;
+            do {
+                i++;
+            } while (arr[i] < pivot);
+            do {
+                j--;
+            } while (arr[j] > pivot);
 
-            System.out.println("Element which most then pivot(or pivot): " + Integer.toString(arr[i]));
-            System.out.println("Element which less then pivot(or pivot): " + Integer.toString(arr[j]));
-            if ((arr[i] >= arr[j]) && (arr[i] != arr[j])) {
-               int temp = arr[i];
-               arr[i] = arr[j];
-               arr[j] = temp;
-               System.out.println("Swap " + Integer.toString(arr[i]) + " -> " + Integer.toString(arr[j]));
-               System.out.println("New array is: " + Arrays.toString(arr));
+            System.out.println("Element which most(or equal) then pivot: " + Integer.toString(arr[i]));
+            System.out.println("Element which less(or equal) then pivot: " + Integer.toString(arr[j]));
+
+            if (i >= j) {
+                return j;
+            } else {
+                swap(arr, i, j);
             }
-
-            if (i >= j)
-            return j;
         }
+    }
 
+    private static void swap(int[] arr, int i, int j) {
+      System.out.println("Swap: " + Integer.toString(arr[i]) + " -> " + Integer.toString(arr[j]));
+      int tmp = arr[i];
+      arr[i] = arr[j];
+      arr[j] = tmp;
     }
 
     private static void printRangeOfArray(int[] arr, int low, int high) {
