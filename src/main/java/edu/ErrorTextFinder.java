e5703b9aa81a3fabccec8b2c297aa0c99f84d4bd
Moroka <supermoroka@gmail.com>
2019-07-24 15:11:06 +0300

    Find error in text #3

--- src/main/java/edu/ErrorTextFinder.java
+++ src/main/java/edu/ErrorTextFinder.java
@@ -4,64 +4,35 @@ public class ErrorTextFinder {
 
     //
     public static boolean isTextContainErrors(String sourceText, String comparedText) {
-        System.out.print("Compare '" + sourceText + "' & '" + comparedText);
-
         final int lengthDiff = sourceText.length() - comparedText.length();
 
         // Get result if length of texts differs by more than 1
-        if (lengthDiff > 1 || lengthDiff < -1) {
-            System.out.println("' Length of texts differs by more than 1");
-            return true;
-        }
-        // Get result if texts are equal
-        if (sourceText.equals(comparedText)) {
-            System.out.println("' Texts are equal");
-            return false;
-        }
-
-        int errorCounter = 0;
-        char[] sourceTextCharArr = sourceText.toCharArray();
-        char[] comparedTextCharArr = comparedText.toCharArray();
+        if (lengthDiff > 1 || lengthDiff < -1) return true;
 
         // If sourceText.length == comparedText.length: compare chars and calculate errors count
         if (lengthDiff == 0) {
+            int errorCounter = 0;
             for (int i = 0; i < sourceText.length() - 1; i++) {
-                if (sourceTextCharArr[i] != comparedTextCharArr[i]) {
+                if (sourceText.charAt(i) != comparedText.charAt(i)) {
                     errorCounter++;
+                    if (errorCounter > 1) return true;
                 }
             }
 
-        // If comparedText has missing chars
-        } else if (lengthDiff == 1) {
-            int j = 0;
-            for (int i = 0; i < sourceText.length() - 1; i++) {
-                if (sourceTextCharArr[i] != comparedTextCharArr[j]) {
-                    errorCounter++;
-                    if (sourceTextCharArr[i+1] == comparedTextCharArr[j]) {
-                        i++;
-                    } else {
-                        errorCounter++;
-                    }
-                }
-                j++;
-            }
-        // ComparedText has extra chars
+        // ComparedText has missing or extra chars
         } else {
-            int j = 0;
             for (int i = 0; i < sourceText.length() - 1; i++) {
-                if (sourceTextCharArr[i] != comparedTextCharArr[j]) {
-                    errorCounter++;
-                    if (sourceTextCharArr[i] == comparedTextCharArr[j+1]) {
-                        j++;
-                    } else {
-                        errorCounter++;
-                    }
+                if (sourceText.charAt(i) != comparedText.charAt(i)) {
+                    // sourceText.length > comparedText.length
+                    if (lengthDiff == -1)
+                        return !(sourceText.substring(i).equals(comparedText.substring(i + 1)));
+                    // sourceText.length < comparedText.length
+                    else
+                        return !(sourceText.substring(i + 1).equals(comparedText.substring(i)));
                 }
-                j++;
             }
         }
 
-        System.out.println("' Errors count: " + errorCounter);
-        return errorCounter > 1;
+        return false;
     }
 }
