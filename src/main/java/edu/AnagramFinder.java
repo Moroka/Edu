2d1f0da3d2fc864199c445fb7a4d6b8d2d1d23b8
Alexey Zavada <supermoroka@gmail.com>
2019-07-29 22:18:56 +0300

    remake algo

--- src/main/java/edu/AnagramFinder.java
+++ src/main/java/edu/AnagramFinder.java
@@ -6,22 +6,28 @@ public class AnagramFinder {
     public static int getAnagramIndexAtText(String anagramText, String text) {
         System.out.println("Find anagram: '" + anagramText + "' at text: '" + text + "'");
 
-        HashMap<Character, Integer> anagram = getAnagram(anagramText);;
+        HashMap<Character, Integer> anagram = getAnagram(anagramText);
 
         System.out.println("Anagram hashMap is: " + anagram);
+        System.out.println();
 
         for (int i = 0; i < text.length(); i++) {
+             if (i > text.length() - 1 - anagram.size()) return -1;
              if (anagram.containsKey(text.charAt(i))) {
-                 for (int j = 0; j < anagramText.length(); j++) {
-                     if (anagram.containsKey(text.charAt(i))) {
-                         removeCharAtHashMap(anagram, text.charAt(i));
-                         System.out.println("Char is: " + text.charAt(i));
-                         System.out.println("Anagram hashMap is: aft remove" + anagram);
-                         System.out.println("Size is: " + anagram.size());
+                 System.out.println("Find anagram symbol '" + text.charAt(i) + "' at index: " + (i));
+                 removeCharAtHashMap(anagram, text.charAt(i));
+                 System.out.println("Anagram hashMap after removing symbol is: " + anagram + " Anagram size is: " + anagram.size());
+                 if (anagram.size() == 0) return i;
+                 for (int j = 1; j < anagramText.length(); j++) {
+                     if (anagram.containsKey(text.charAt(i + j))) {
+                         System.out.println("Find anagram symbol '" + text.charAt(i+j) + "' at index: " + (i+j));
+                         removeCharAtHashMap(anagram, text.charAt(i+j));
+                         System.out.println("Anagram hashMap after removing symbol is: " + anagram + " Anagram size is: " + anagram.size());
                          if (anagram.size() == 0) return i;
                      } else {
-                         i = i - j;
+                         System.out.println("Find non-anagram symbol '" + text.charAt(i+j) + "' at index: " + (i + j));
                          anagram = getAnagram(anagramText);
+                         System.out.println(":: Refreshing anagram... " + anagram);
                          break;
                      }
                  }
--- src/test/java/edu/AnagramFinderTest.java
+++ src/test/java/edu/AnagramFinderTest.java
@@ -7,16 +7,14 @@ import static org.junit.Assert.*;
 public class AnagramFinderTest {
     @Test
     public void anagramFinderTest() {
+        assertEquals(-1, AnagramFinder.getAnagramIndexAtText("rest",
+                "simple test"));
         assertEquals(7, AnagramFinder.getAnagramIndexAtText("ttse",
                 "simple test"));
         assertEquals(8, AnagramFinder.getAnagramIndexAtText("ttse",
                 "simple stest"));
-        assertEquals(41, AnagramFinder.getAnagramIndexAtText("yaes",
-                "If you want to change default branch, it's so easy with github"));
-    }
-
-    @Test
-    public void aTest() {
+        assertEquals(8, AnagramFinder.getAnagramIndexAtText("yaes",
+                "It's so easy with github"));
         assertEquals(-1, AnagramFinder.getAnagramIndexAtText("ttse",
                 "simple trest"));
     }
