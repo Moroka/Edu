505d23ddd350c5d39425a9b9668741423103bf47
Alexey Zavada <supermoroka@gmail.com>
2019-07-31 00:11:01 +0300

    AnagramFinder: wip

--- src/main/java/edu/AnagramFinder.java
+++ src/main/java/edu/AnagramFinder.java
@@ -11,26 +11,47 @@ public class AnagramFinder {
         System.out.println("Anagram hashMap is: " + anagram);
         System.out.println();
 
+        int originalSize = anagram.size();
+        boolean reverseMode = false;
+        int matchesCount = 0;
+
         for (int i = 0; i < text.length(); i++) {
-             if (i > text.length() - 1 - anagram.size()) return -1;
-             if (anagram.containsKey(text.charAt(i))) {
+             if (i > text.length() - anagram.size()) return -1;
+
+             if (reverseMode) {
+                 matchesCount = matchesCount - 1;
+                 addCharToHashMap(anagram, text.charAt(i));
+                 if (matchesCount == 0) {
+                     reverseMode = false;
+                     i = i+ 1;
+                 }
+
+             }
+
+             if (anagram.containsKey(text.charAt(i)) && !reverseMode) {
                  System.out.println("Find anagram symbol '" + text.charAt(i) + "' at index: " + (i));
                  removeCharAtHashMap(anagram, text.charAt(i));
-                 System.out.println("Anagram hashMap after removing symbol is: " + anagram + " Anagram size is: " + anagram.size());
-                 if (anagram.size() == 0) return i;
-                 for (int j = 1; j < anagramText.length(); j++) {
-                     if (anagram.containsKey(text.charAt(i + j))) {
-                         System.out.println("Find anagram symbol '" + text.charAt(i+j) + "' at index: " + (i+j));
-                         removeCharAtHashMap(anagram, text.charAt(i+j));
-                         System.out.println("Anagram hashMap after removing symbol is: " + anagram + " Anagram size is: " + anagram.size());
-                         if (anagram.size() == 0) return i;
-                     } else {
-                         System.out.println("Find non-anagram symbol '" + text.charAt(i+j) + "' at index: " + (i + j));
-                         anagram = getAnagram(anagramText);
-                         System.out.println(":: Refreshing anagram... " + anagram);
-                         break;
-                     }
+                 matchesCount = matchesCount + 1;
+                 System.out.println("Anagram hashMap after removing symbol is: " + anagram + " Anagram size is: " + anagram.size() + " Matches count: " + matchesCount);
+                 if (anagram.size() == 0) return i - originalSize + 1;
+             else if (matchesCount > 0) {
+                 i = i - matchesCount;
+                 reverseMode = true;
                  }
+
+//                 for (int j = 1; j < anagramText.length(); j++) {
+//                     if (anagram.containsKey(text.charAt(i + j))) {
+//                         System.out.println("Find anagram symbol '" + text.charAt(i+j) + "' at index: " + (i+j));
+//                         removeCharAtHashMap(anagram, text.charAt(i+j));
+//                         System.out.println("Anagram hashMap after removing symbol is: " + anagram + " Anagram size is: " + anagram.size());
+//
+//                     } else {
+//                         System.out.println("Find non-anagram symbol '" + text.charAt(i+j) + "' at index: " + (i + j));
+//                         anagram = getAnagram(anagramText);
+//                         System.out.println(":: Refreshing anagram... " + anagram);
+//                         break;
+//                     }
+//                 }
             }
         }
 
@@ -50,11 +71,19 @@ public class AnagramFinder {
         return hashMap;
     }
 
-    private static void removeCharAtHashMap(HashMap<Character, Integer> hashMap, Character j) {
-        if (hashMap.get(j) > 1) {
-            hashMap.put(j, hashMap.get(j)- 1);
+    private static void removeCharAtHashMap(HashMap<Character, Integer> hashMap, Character c) {
+        if (hashMap.get(c) > 1) {
+            hashMap.put(c, hashMap.get(c) - 1);
         } else {
-            hashMap.remove(j);
+            hashMap.remove(c);
+        }
+    }
+
+    private static void addCharToHashMap(HashMap<Character, Integer> hashMap, Character c) {
+        if (hashMap.containsKey(c)) {
+            hashMap.put(c, hashMap.get(c) + 1);
+        } else {
+            hashMap.put(c, 1);
         }
     }
 }
--- src/test/java/edu/AnagramFinderTest.java
+++ src/test/java/edu/AnagramFinderTest.java
@@ -6,16 +6,27 @@ import static org.junit.Assert.*;
 
 public class AnagramFinderTest {
     @Test
-    public void anagramFinderTest() {
-        assertEquals(-1, AnagramFinder.getAnagramIndexAtText("rest",
-                "simple test"));
-        assertEquals(7, AnagramFinder.getAnagramIndexAtText("ttse",
-                "simple test"));
-        assertEquals(8, AnagramFinder.getAnagramIndexAtText("ttse",
-                "simple stest"));
-        assertEquals(8, AnagramFinder.getAnagramIndexAtText("yaes",
-                "It's so easy with github"));
-        assertEquals(-1, AnagramFinder.getAnagramIndexAtText("ttse",
-                "simple trest"));
+    public void anagramFinderTest1() {
+        assertEquals(-1, AnagramFinder.getAnagramIndexAtText("rest", "simple test"));
+    }
+
+    @Test
+    public void anagramFinderTest2() {
+        assertEquals(1, AnagramFinder.getAnagramIndexAtText("rat", "part"));
+    }
+
+    @Test
+    public void anagramFinderTest3() {
+        assertEquals(8, AnagramFinder.getAnagramIndexAtText("test", "tesqqqtestqqqq"));
+    }
+
+    @Test
+    public void anagramFinderTest4() {
+        assertEquals(8, AnagramFinder.getAnagramIndexAtText("yaes", "It's so easy with github"));
+    }
+
+    @Test
+    public void anagramFinderTest5() {
+        assertEquals(-1, AnagramFinder.getAnagramIndexAtText("ttse", "simple trest"));
     }
 }
