4c956b57fea3bd16c1cdfcb0958448f153f262c4
Moroka <supermoroka@gmail.com>
2019-07-31 14:30:06 +0300

    Find typo in text

--- src/main/java/edu/TypoTextFinder.java
+++ src/main/java/edu/TypoTextFinder.java
@@ -1,24 +1,20 @@
 package edu;
 
-public class ErrorTextFinder {
-
-    //
+public class TypoTextFinder {
     public static boolean hasMaxOneTypo(String s1, String s2) {
         final int lengthDiff = s1.length() - s2.length();
 
-        // Get result if length of texts differs by more than 1
-        if (lengthDiff > 1 || lengthDiff < -1) return false;
-
-        // If lengths of texts are equal
-        if (lengthDiff == 0) {
-            return checkEqualLengths(s1, s2);
-        } else return checkDifferentLengths(s1, s2);
+        switch (lengthDiff) {
+            case -1: return checkFirstLonger(s2, s1);
+            case 0: return checkEqualLengths(s1, s2);
+            case 1: return checkFirstLonger(s1, s2);
+            default: return false;
+        }
     }
 
     private static boolean checkEqualLengths(String s1, String s2) {
         for (int i = 0; i < s1.length() - 1; i++) {
             if (s1.charAt(i) != s2.charAt(i)) {
-                // After finding first mismatch - check remaining tails
                 if (i + 1 <= s1.length() - 1)
                     return tailsEqual(s1, i + 1, s2, i + 1);
             }
@@ -26,14 +22,12 @@ public class ErrorTextFinder {
         return true;
     }
 
-    private static boolean checkDifferentLengths(String s1, String s2) {
+    private static boolean checkFirstLonger(String s1, String s2) {
         for (int i = 0; i < s1.length(); i++) {
-            if (s1.charAt(i) != s2.charAt(i)) {
-                if (tailsEqual(s1, i + 1, s2, i)) return true;
-                return (tailsEqual(s1, i, s2, i + 1));
-            }
-
-            if (i == s2.length() - 1) return true;
+            if (s1.charAt(i) != s2.charAt(i))
+                return (tailsEqual(s1, i + 1, s2, i));
+            if (i == s2.length() - 1)
+                return true;
         }
         return true;
     }
