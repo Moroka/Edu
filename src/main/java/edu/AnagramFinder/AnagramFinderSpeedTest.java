5547091c5f997a88f400ab314196e4f6aaaf1cfd
Alexey Zavada <supermoroka@gmail.com>
2019-08-14 01:40:56 +0300

    AnagramFinder: Refactoring

--- src/main/java/edu/AnagramFinder/AnagramFinderSpeedTest.java
+++ src/main/java/edu/AnagramFinder/AnagramFinderSpeedTest.java
@@ -3,17 +3,16 @@ package edu.AnagramFinder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Arrays;
 import java.util.Random;
 
 public class AnagramFinderSpeedTest {
-    private static final Random random = new Random();
+    private final Random random = new Random();
     private static final Logger LOGGER = LoggerFactory.getLogger(AnagramFinderSpeedTest.class);
     private static final int MAX_TEXT_LENGTH = 0x10000;
     private static final int MAX_ANAGRAM_LENGTH = 0x100;
     private static final int ITERATION_COUNT = 0x1000;
     private static final int WARM_UP_ITERATION_COUNT = 0x100;
-    private long anagramFinderTotalTime;
-    private long anagramFinderFastTotalTime;
 
     public static void main(String[] args) {
         AnagramFinderSpeedTest speedTest = new AnagramFinderSpeedTest();
@@ -30,9 +29,9 @@ public class AnagramFinderSpeedTest {
         }
 
         // Check 1st algorithm
-        for (AnagramFinderSpeedTestData data : testData) {
-            anagramFinderTotalTime += measureTime(() -> AnagramFinder.getAnagramIndexAtText(data.anagramText, data.text));
-        }
+        final long anagramFinderTotalTime = Arrays.stream(testData)
+                .map((data) -> measureTime(() -> AnagramFinder.getAnagramIndexAtText(data.anagramText, data.text)))
+                .reduce(0L, Long::sum);
 
         // Warm up 2nd algorithm
         for (AnagramFinderSpeedTestData data : warmUpData) {
@@ -40,9 +39,9 @@ public class AnagramFinderSpeedTest {
         }
 
         // Check 2nd algorithm
-        for (AnagramFinderSpeedTestData data : testData) {
-            anagramFinderFastTotalTime += measureTime(() -> AnagramFinderFast.getAnagramIndexAtText(data.anagramText, data.text));
-        }
+        final long anagramFinderFastTotalTime = Arrays.stream(testData)
+                .map((data) -> measureTime(() -> AnagramFinder.getAnagramIndexAtText(data.anagramText, data.text)))
+                .reduce(0L, Long::sum);
 
         printMeasureTime("[AnagramFinder] Total runtime is:", anagramFinderTotalTime);
         printMeasureTime("[AnagramFinder] Average runtime is:", anagramFinderTotalTime / ITERATION_COUNT);
@@ -50,7 +49,7 @@ public class AnagramFinderSpeedTest {
         printMeasureTime("[AnagramFinderFast] Average runtime is:", anagramFinderFastTotalTime / ITERATION_COUNT);
     }
 
-    private static AnagramFinderSpeedTestData[] generateTestData(int size) {
+    private AnagramFinderSpeedTestData[] generateTestData(int size) {
         final AnagramFinderSpeedTestData[] dataArray = new AnagramFinderSpeedTestData[size];
         for (int i = 0; i < size; i++) {
             dataArray[i] = new AnagramFinderSpeedTestData(getRandomString(MAX_ANAGRAM_LENGTH), getRandomString(MAX_TEXT_LENGTH));
@@ -58,7 +57,7 @@ public class AnagramFinderSpeedTest {
         return dataArray;
     }
 
-    private static String getRandomString(int maxLength) {
+    private String getRandomString(int maxLength) {
         final int randomLength = random.nextInt(maxLength);
         final StringBuilder s = new StringBuilder(randomLength);
         for (int i = 0; i < randomLength; i++) {
