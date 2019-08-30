d41c16b2ea36ad6ca40601024198b60b1279ae91
Moroka <supermoroka@gmail.com>
2019-08-30 12:50:19 +0300

    binarytree tests

--- src/main/java/edu/BinaryTree/BinaryTreeHelper.java
+++ src/main/java/edu/BinaryTree/BinaryTreeHelper.java
@@ -3,19 +3,24 @@ package edu.binarytree;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class BinaryTreeHelper {
+public final class BinaryTreeHelper {
     private static final Logger LOGGER = LoggerFactory.getLogger(BinaryTreeHelper.class);
     private String str;
     private String treeToString;
     private int strIndex;
 
     public static BinaryTreeNode createTree(String s) {
-        LOGGER.debug("Create tree from '{}'", s);
+        if (!canCreateTree(s)) {
+            LOGGER.debug("Invalid input text: '{}'", s);
+            return null;
+        } else {
+            LOGGER.debug("Create tree from '{}'", s.charAt(s.length() - 2));
 
-        BinaryTreeHelper helper = new BinaryTreeHelper();
-        helper.str = s;
-        helper.strIndex = 0;
-        return helper.createTreeRecursive();
+            BinaryTreeHelper helper = new BinaryTreeHelper();
+            helper.str = s;
+            helper.strIndex = 0;
+            return helper.createTreeRecursive();
+        }
     }
 
     public static String treeToString(BinaryTreeNode node) {
@@ -51,5 +56,11 @@ public class BinaryTreeHelper {
         treeToString = treeToStringRecursive(tree.getLeftNode());
         treeToString = treeToStringRecursive(tree.getRightNode());
         return treeToString;
+    }
+
+    private static boolean canCreateTree(String s) {
+        if (s.length() < 3)
+            return false;
+        return s.substring(s.length() - 2).equals("__");
     }
 }
--- src/test/java/edu/BinaryTree/BinaryTreeTest.java
+++ src/test/java/edu/BinaryTree/BinaryTreeTest.java
@@ -2,15 +2,66 @@ package edu.binarytree;
 
 import org.junit.Test;
 
+
+import static org.junit.Assert.*;
+
 public class BinaryTreeTest {
     @Test
-    public void repetitiveCharsTest() {
+    public void oneNodeTreeTest() {
+        /*
+                  a
+        */
+        String inputString = "a__";
+        BinaryTreeNode node = BinaryTreeHelper.createTree(inputString);
+        assertEquals(inputString, BinaryTreeHelper.treeToString(node));
+    }
+
+    @Test
+    public void twoNodesTreeTest() {
+        /*
+                  a
+              b
+        */
+        String inputString = "ab___";
+        BinaryTreeNode node = BinaryTreeHelper.createTree(inputString);
+        assertEquals(inputString, BinaryTreeHelper.treeToString(node));
+    }
+
+
+    @Test
+    public void threeNodesTreeTest() {
+        /*
+                  a
+             b          c
+        */
+        String inputString = "ab__c__";
+        BinaryTreeNode node = BinaryTreeHelper.createTree(inputString);
+        assertEquals(inputString, BinaryTreeHelper.treeToString(node));
+    }
+
+    @Test
+    public void repetitiveNodesTest() {
         /*
                   a
              b          c
            d   e      g   a
         */
-        BinaryTreeNode node = BinaryTreeHelper.createTree("abd__e__cg__a__");
-        System.out.println(BinaryTreeHelper.treeToString(node));
+        String inputString = "abd__e__cg__a__";
+        BinaryTreeNode node = BinaryTreeHelper.createTree(inputString);
+        assertEquals(inputString, BinaryTreeHelper.treeToString(node));
+    }
+
+    @Test
+    public void tooShortTreeTest() {
+        String inputString = "__";
+        BinaryTreeNode node = BinaryTreeHelper.createTree(inputString);
+        assertNull(node);
+    }
+
+    @Test
+    public void invalidEndTreeTest() {
+        String inputString = "ab_";
+        BinaryTreeNode node = BinaryTreeHelper.createTree(inputString);
+        assertNull(node);
     }
 }
