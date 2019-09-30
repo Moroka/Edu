ecbf09ab57876ceeca56164349c68512a47de185
Alexey Zavada <supermoroka@gmail.com>
2019-09-30 22:04:06 +0300

    monitoringSystem: add flexible

--- src/main/java/edu/monitoringSystem/MonitoringSystemEventDispatcher.java
+++ src/main/java/edu/monitoringSystem/MonitoringSystemEventDispatcher.java
@@ -3,26 +3,11 @@ package edu.monitoringSystem;
 import java.util.Random;
 
 public class MonitoringSystemEventDispatcher {
-
-
     public static void sendRandomEventToMonitoring(int count, IMonitoringSystemHandler monitoringInstance) {
         final Random random = new Random();
         for (int i = 0; i < count; i++) {
-            final int eventType = random.nextInt(3);
-            switch (eventType) {
-                case 0: {
-                    monitoringInstance.handleEvent(MonitoringSystemEventType.HIGH_LOAD);
-                    break;
-                }
-                case 1: {
-                    monitoringInstance.handleEvent(MonitoringSystemEventType.ERROR);
-                    break;
-                }
-                case 2: {
-                    monitoringInstance.handleEvent(MonitoringSystemEventType.CONNECTION_REFUSED);
-                    break;
-                }
-            }
+            final int eventType = random.nextInt(MonitoringSystemEventType.values().length);
+            monitoringInstance.handleEvent(MonitoringSystemEventType.values()[eventType]);
         }
     }
 }
--- src/main/java/edu/monitoringSystem/MonitoringSystemHandler.java
+++ src/main/java/edu/monitoringSystem/MonitoringSystemHandler.java
@@ -4,22 +4,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.sql.Timestamp;
-import java.util.Date;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.*;
 
 public class MonitoringSystemHandler implements IMonitoringSystemHandler {
     private static final int NANOSECONDS_TIME_TO_KEEP = 2;
-
-    private static final String HIGH_LOAD_EVENTS = "[HIGH_LOAD]";
-    private static final String ERROR_EVENTS = "[ERROR]";
-    private static final String CONNECTION_REFUSED_EVENTS = "[CONNECTION_REFUSED_EVENTS]";
-
     private static final Logger LOGGER = LoggerFactory.getLogger(MonitoringSystemHandler.class);
-
-    private static final Queue<Timestamp> highLoadEvents = new LinkedList<>();
-    private static final Queue<Timestamp> errorEvents = new LinkedList<>();
-    private static final Queue<Timestamp> connectionRefusedEvents = new LinkedList<>();
+    private static final Map<String, Queue<Timestamp>> hashMap = new HashMap<>();
 
     public MonitoringSystemHandler() {
     }
@@ -28,32 +18,33 @@ public class MonitoringSystemHandler implements IMonitoringSystemHandler {
         final Date now = new Date();
         final Timestamp timestamp = new Timestamp(now.getTime());
 
-        if (event == MonitoringSystemEventType.HIGH_LOAD) {
-            removeOutdatedEvents(HIGH_LOAD_EVENTS, highLoadEvents, timestamp);
-            highLoadEvents.add(timestamp);
-            LOGGER.info("{} Add event with timestamp: {}", HIGH_LOAD_EVENTS, timestamp.getTime());
-        } else if (event == MonitoringSystemEventType.ERROR) {
-            removeOutdatedEvents(ERROR_EVENTS, errorEvents, timestamp);
-            errorEvents.add(timestamp);
-            LOGGER.info("{} Add event with timestamp: {}", ERROR_EVENTS, timestamp.getTime());
-        } else {
-            removeOutdatedEvents(CONNECTION_REFUSED_EVENTS, connectionRefusedEvents, timestamp);
-            connectionRefusedEvents.add(timestamp);
-            LOGGER.info("{} Add event with timestamp: {}", CONNECTION_REFUSED_EVENTS, timestamp.getTime());
+        Queue<Timestamp> events = hashMap.get(event.name());
+        if (events != null)
+            hashMap.put(event.name(), addEvent(event.name(), events, timestamp));
+        else {
+            Queue<Timestamp> queue = new LinkedList<>();
+            hashMap.put(event.name(), addEvent(event.name(), queue, timestamp));
         }
     }
 
     public void printStat() {
         LOGGER.info("----------------------------------");
-        LOGGER.info("Statistics for last {} nanoseconds", NANOSECONDS_TIME_TO_KEEP);
-        LOGGER.info("{} Events count: {}", HIGH_LOAD_EVENTS, highLoadEvents.size());
-        LOGGER.info("{} Events count: {}", ERROR_EVENTS, errorEvents.size());
-        LOGGER.info("{} Events count: {}", CONNECTION_REFUSED_EVENTS, connectionRefusedEvents.size());
+        LOGGER.info("Statistics for last {} nanoseconds:", NANOSECONDS_TIME_TO_KEEP);
+        for (Map.Entry<String, Queue<Timestamp>> entry : hashMap.entrySet()) {
+            LOGGER.info("[{}] Events count: {}", entry.getKey(), entry.getValue().size());
+        }
     }
 
-    private static void removeOutdatedEvents(String queueDescriptor, Queue<Timestamp> queue, Timestamp timestamp) {
+    private static Queue<Timestamp> addEvent(String eventDescriptor, Queue<Timestamp> queue, Timestamp timestamp) {
+        removeOutdatedEvents(eventDescriptor, queue, timestamp);
+        queue.add(timestamp);
+        LOGGER.info("[{}] Add event with timestamp: {}", eventDescriptor, timestamp.getTime());
+        return queue;
+    }
+
+    private static void removeOutdatedEvents(String eventDescriptor, Queue<Timestamp> queue, Timestamp timestamp) {
         while ((queue.size() > 0) && (timestamp.getTime() - queue.peek().getTime()) > NANOSECONDS_TIME_TO_KEEP) {
-            LOGGER.info("{} Remove event with timestamp: {}", queueDescriptor, queue.peek().getTime());
+            LOGGER.info("[{}] Remove event with timestamp: {}", eventDescriptor, queue.peek().getTime());
             queue.remove();
         }
     }
