c28ecb9fcca519222bb76431f09813f0b51b5c6d
Moroka <supermoroka@gmail.com>
2019-11-06 13:51:33 +0300

    binaryTree

--- src/main/java/edu/binarytree/BinaryTreeCharStorage.java
+++ src/main/java/edu/binarytree/BinaryTreeCharStorage.java
@@ -5,17 +5,17 @@ import org.slf4j.LoggerFactory;
 
 import java.util.HashMap;
 
-public final class BinaryTreeCharStorage {
+final class BinaryTreeCharStorage {
     private static final Logger LOGGER = LoggerFactory.getLogger(BinaryTreeCharStorage.class);
     private final BinaryTreeNode tree;
-    private BinaryTreeNode[] result = new BinaryTreeNode[2];
-    private HashMap<Integer, BinaryTreeNode> storage = new HashMap<>();
+    private final BinaryTreeNode[] result = new BinaryTreeNode[2];
+    private final HashMap<Integer, BinaryTreeNode> storage = new HashMap<>();
 
-    public BinaryTreeCharStorage(BinaryTreeNode tree) {
+    BinaryTreeCharStorage(BinaryTreeNode tree) {
         this.tree = tree;
     }
 
-    public BinaryTreeNode[] sameCharSet() {
+    BinaryTreeNode[] sameCharSet() {
         sameCharSet(tree);
         return result;
     }
@@ -33,23 +33,23 @@ public final class BinaryTreeCharStorage {
 
         LOGGER.info(":::::::::::: Processing node: {}", node.getValue());
 
-        final int rawId = 1 << (node.getValue() - 'a');
-        LOGGER.info("Node raw binary value: {}", Integer.toBinaryString(rawId));
+        final int incompleteBinaryRepresentation = 1 << (node.getValue() - 'a');
+        LOGGER.info("Node binary value without sub-nodes: {}", Integer.toBinaryString(incompleteBinaryRepresentation));
         LOGGER.info("Left id: {}.", Integer.toBinaryString(leftId));
         LOGGER.info("Right id: {}.", Integer.toBinaryString(rightId));
-        final int id = rawId | leftId | rightId;
-        LOGGER.info("New node id after merging sub-nodes: {}", Integer.toBinaryString(id));
+        final int binaryRepresentation = incompleteBinaryRepresentation | leftId | rightId;
+        LOGGER.info("New node id after merging sub-nodes: {}", Integer.toBinaryString(binaryRepresentation));
 
-        final BinaryTreeNode value = storage.get(id);
+        final BinaryTreeNode value = storage.get(binaryRepresentation);
         if (value == null) {
-            LOGGER.info("Add node '{}' to storage with id: {}", node.getValue(), Integer.toBinaryString(id));
-            storage.put(id, node);
+            LOGGER.info("Add node '{}' to storage with id: {}", node.getValue(), Integer.toBinaryString(binaryRepresentation));
+            storage.put(binaryRepresentation, node);
         } else {
             LOGGER.info("Finding nodes with equal ids: {} {}", node.getValue(), value.getValue());
             result[0] = node;
             result[1] = value;
         }
 
-        return id;
+        return binaryRepresentation;
     }
 }
